1c792397c6dec8e80e22654676e5f4baa111ceef
moch1772 <ziggs1772@gmail.com>
2020-10-15 15:20:33 +0200

    Search works

--- Karl/search.php
+++ Karl/search.php
@@ -1,3 +1,6 @@
+<?php
+    require '../bloggpost.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,27 +10,62 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $studentbase = "provider";
+
     
-    // Skapa kopling till server
-    $mysqli = new mysqli($servername,$username,$password,$studentbase);
-
-    if (!$mysqli) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    $search=
 
 
-    $sql="select * from post where title='$search' "
-    $post= $mysqli->query($sql);
-    while($row = $post->fetch_assoc()){
-        
-    }*/
+    <table border="2">
+  <tr>
+    <td>postID</td>
+    <td>anvID</td>
+    <td>Title</td>
+    <td>Text</td>
+    <td>show Comment</td>
+    <td>Edit</td>
+    <td>Delete</td>
+  </tr>
+
+<?php
+
+include "../dbsetup.php";
+$search=$_POST['search'];
+$result = mysqli_query($conn, "SELECT * FROM post where title like '%$search%'");
+while($row = mysqli_fetch_array($result))
+{
 ?>
+    <tr>
+        <td><?php echo $row['postID']; ?></td>
+        <td><?php echo $row['userID']; ?></td>
+        <td><?php echo $row['title']; ?></td>
+        <td><?php echo $row['text']; ?></td>
+        <td><?php echo $row['showComments']; ?></td>
+        <td><a href="editpost.php?postID=<?php echo $row['postID']; ?>">Edit</a></td>   
+        <td><a href="deletepost.php?postID=<?php echo $row['postID']; ?>">Delete</a></td>
+    </tr>	
+<?php
+    }
+    $search=$_POST['search'];
+    $tags = mysqli_query($conn, "SELECT postID FROM tag where tag='$search'");
+    while($rows = mysqli_fetch_array($tags))
+    {   
+        $result = mysqli_query($conn, "SELECT * FROM post where postID=".$rows['postID']."");
+        while($row = mysqli_fetch_array($result))
+        {?>
+            <tr>
+            <td><?php echo $row['postID']; ?></td>
+            <td><?php echo $row['userID']; ?></td>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php echo $row['text']; ?></td>
+            <td><?php echo $row['showComments']; ?></td>
+            <td><a href="editpost.php?postID=<?php echo $row['postID']; ?>">Edit</a></td>   
+            <td><a href="deletepost.php?postID=<?php echo $row['postID']; ?>">Delete</a></td>
+            </tr>
+        <?php
+        }
+    }
+?>
+</table>
+<a href="search.html">BACK</a>
 
 
 
