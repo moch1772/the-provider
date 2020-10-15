0081a909e336c2d8a27d59dd5836aeacf37bad16
moch1772 <ziggs1772@gmail.com>
2020-10-15 15:30:27 +0200

    Update search.php

--- Karl/search.php
+++ Karl/search.php
@@ -44,11 +44,28 @@ while($row = mysqli_fetch_array($result))
     </tr>	
 <?php
     }
-    $search=$_POST['search'];
     $tags = mysqli_query($conn, "SELECT postID FROM tag where tag='$search'");
     while($rows = mysqli_fetch_array($tags))
     {   
         $result = mysqli_query($conn, "SELECT * FROM post where postID=".$rows['postID']."");
+        while($row = mysqli_fetch_array($result))
+        {?>
+            <tr>
+            <td><?php echo $row['postID']; ?></td>
+            <td><?php echo $row['userID']; ?></td>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php echo $row['text']; ?></td>
+            <td><?php echo $row['showComments']; ?></td>
+            <td><a href="editpost.php?postID=<?php echo $row['postID']; ?>">Edit</a></td>   
+            <td><a href="deletepost.php?postID=<?php echo $row['postID']; ?>">Delete</a></td>
+            </tr>
+        <?php
+        }
+    }
+    $auther = mysqli_query($conn, "SELECT ID FROM user where name='$search'");
+    while($rows = mysqli_fetch_array($auther))
+    {   
+        $result = mysqli_query($conn, "SELECT * FROM post where userID=".$rows['ID']."");
         while($row = mysqli_fetch_array($result))
         {?>
             <tr>
