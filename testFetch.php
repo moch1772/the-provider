dca306ab057b79ec50a7210e2f227b5938b61736
nibarsito <nibar@hotmail.se>
2020-10-26 14:48:00 +0100

    ett exempel av hur en användarehämtar rapporter

---
+++ testFetch.php
@@ -0,0 +1,8 @@
+<?php
+$curl=curl_init();
+curl_setopt($curl, CURLOPT_URL, "http://localhost:8080/pt/the-provider/fetchReport.php?insertReport=test");
+curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
+$result=curl_exec($curl);
+curl_close($curl);
+echo $result;
+?>
