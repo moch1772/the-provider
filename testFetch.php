217a061c25b77f42567b49327e36802255a3056a
nibarsito <nibar@hotmail.se>
2020-10-28 13:19:11 +0100

    updaterad kod

--- testFetch.php
+++ testFetch.php
@@ -1,6 +1,6 @@
 <?php
 $curl=curl_init();
-curl_setopt($curl, CURLOPT_URL, "http://localhost:8080/pt/the-provider/fetchReport.php?insertReport=test");
+curl_setopt($curl, CURLOPT_URL, "http://localhost:8080/pt/the-provider/fetchReport.php?insertReport=test);
 curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
 $result=curl_exec($curl);
 curl_close($curl);
