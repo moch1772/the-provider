1d2edb037b2b11c918b4579b81a69bbd00b9a652
nibarsito <nibar@hotmail.se>
2020-10-29 13:53:55 +0100

    nu är rapporten klar. fortfarande en funktion som behöver göras

---
+++ models/report.php
@@ -0,0 +1,58 @@
+<?php    
+header("Content-Type:application/json");
+class Report
+{
+public function fetchReport($conn)
+{
+$query="select * from report";
+    if($result=$conn->query($query))
+    {
+$array=array();
+while($row=$result->fetch(PDO::FETCH_ASSOC))
+{
+  foreach($row as $i)
+  $array[]=$i;
+}
+$json=json_encode($array);
+      
+    }
+  return $json;  
+}
+public function insertReport($conn, $postId, $email, $description)
+{
+    if(isset($email)&&isset($description)&&isset($postId))
+    {
+     $query=$conn->prepare("insert into report (PostID, email, description) values(?, ?, ?)");
+     
+  
+     if($query->execute([$postId, $email, $description]))      
+
+     return TRUE;   
+    }
+}
+public function insertReportComment($conn, $commentId, $email, $description)
+{
+    if(isset($email)&&isset($description)&&isset($commentId))
+    {
+     $query=$conn->prepare("insert into report (commentID, email, description) values(?, ?, ?)");
+     if($query->execute([$commentId, $email, $description]))      
+     return TRUE;    
+     else
+     {
+       return FALSE;
+     }
+    }  
+}
+public function insertReportStatus($conn, $reportId , $status)
+{
+    if(isset($reportId))
+    {
+        $query=$conn->prepare("update report set resolved=? where reportID=?");
+      if($query->execute([$reportId, $status]))      
+    return TRUE;        
+  }
+}
+
+}
+
+?>
